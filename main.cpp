15229195dd89537148abb5e757a14ce6b5c4222f
Zer <livefatezz@gmail.com>
2024-08-01 23:08:21 +0800

    fix: dynamic project directory

--- main.cpp
+++ main.cpp
@@ -14,38 +14,36 @@ void testRouter(void)
 
 void testAutoindex(void)
 {
-    Request req("GET / HTTP/1.1\r\nHost: localhost:8080\r\n\r\n");
-    Response res;
-    AutoindexHandler autoindexHandler;
-    RouteDetails routeDetails;
-    routeDetails.root = "/Users/user/sidess/webserv/public";
-    autoindexHandler.handleRequest(req, res, routeDetails, "/Users/user/sidess/webserv/public");
-    std::cout << res.toString() << std::endl;
+	Request req("GET / HTTP/1.1\r\nHost: localhost:8080\r\n\r\n");
+	Response res;
+	AutoindexHandler autoindexHandler;
+	RouteDetails routeDetails;
+	routeDetails.root = "/Users/user/sidess/webserv/public";
+	autoindexHandler.handleRequest(req, res, routeDetails, "/Users/user/sidess/webserv/public");
+	std::cout << res.toString() << std::endl;
 }
 
 void testWebserver(void)
 {
-    // WebServer webserver(PORT, "localhost", "Webserver");
-    
-    // webserver.start();
-    ConfigParser configParser;
-    std::vector<RequestHandler*> requestHandlers;
-    requestHandlers.push_back(new AutoindexHandler());
-    requestHandlers.push_back(new StaticFileHandler());
-    std::vector<ServerBlock*> serverBlocks;
+	// WebServer webserver(PORT, "localhost", "Webserver");
 
-    
+	// webserver.start();
+	ConfigParser configParser;
+	std::vector<RequestHandler *> requestHandlers;
+	requestHandlers.push_back(new AutoindexHandler());
+	requestHandlers.push_back(new StaticFileHandler());
+	std::vector<ServerBlock *> serverBlocks;
 
-    configParser.createServerBlocksFromConf("conf/tester.conf", requestHandlers,  serverBlocks);
-    for (std::vector<ServerBlock*>::iterator it = serverBlocks.begin(); it != serverBlocks.end(); it++)
-    {
-        configParser.displayConfig(**it);
-    }
-
+	configParser.createServerBlocksFromConf("conf/tester.conf", requestHandlers, serverBlocks);
+	for (std::vector<ServerBlock *>::iterator it = serverBlocks.begin(); it != serverBlocks.end(); it++)
+	{
+		configParser.displayConfig(**it);
+	}
 
 	Webserver webserver(serverBlocks);
 	webserver.start();
 }
+
 int main(void)
 {
 	// testRouter();
--- src/Router.cpp
+++ src/Router.cpp
@@ -6,7 +6,7 @@
 /*   By: chenlee <chenlee@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 18:58:14 by chenlee           #+#    #+#             */
-/*   Updated: 2024/06/18 23:16:01 by chenlee          ###   ########.fr       */
+/*   Updated: 2024/08/01 23:07:27 by chenlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,96 +15,122 @@
 #include <iostream>
 #include <iomanip>
 
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#include <limits.h>
+#endif
+
 Router::Router() : _logger(Logger("Router"))
 {
-    _projectDir = "/home/sirhcofe/Core/webserv/";
+	char cwd[PATH_MAX];
+#ifdef _WIN32
+	if (GetCurrentDirectory(PATH_MAX, cwd) != 0)
+	{
+		_projectDir = cwd;
+	}
+	else
+	{
+		printf("Failed to get project directory: %lu\n", GetLastError());
+	}
+#else
+	if (getcwd(cwd, sizeof(cwd)) != NULL)
+	{
+		_projectDir = cwd;
+	}
+	else
+	{
+		perror("Failed to get project directory");
+	}
+#endif
 }
 
 Router::Router(std::string projectDir) : _projectDir(projectDir), _logger(Logger("Router")) {}
 
 Router::~Router()
 {
-    for (std::map<std::string, RouteDetails *>::iterator it = this->_routeTable.begin(); it != this->_routeTable.end(); it++)
-    {
-        delete it->second;
-    }
-    this->_routeTable.clear();
+	for (std::map<std::string, RouteDetails *>::iterator it = this->_routeTable.begin(); it != this->_routeTable.end(); it++)
+	{
+		delete it->second;
+	}
+	this->_routeTable.clear();
 }
 
 void Router::addRoute(const std::string &path, RouteDetails *routeDetail)
 {
-    this->_routeTable[path] = routeDetail;
+	this->_routeTable[path] = routeDetail;
 }
 
 void Router::assignHandlers(requestHandlerVec &handlers)
 {
-    this->_requestHandlers = &handlers;
+	this->_requestHandlers = &handlers;
 }
 
 bool checkAllowedMethods(std::string reqMethods, int allowedMethods)
 {
-    if (reqMethods == "GET")
-    {
-        return allowedMethods & GET;
-    }
-    else if (reqMethods == "POST")
-    {
-        return allowedMethods & POST;
-    }
-    else if (reqMethods == "DELETE")
-    {
-        return allowedMethods & DELETE;
-    }
-    return false;
+	if (reqMethods == "GET")
+	{
+		return allowedMethods & GET;
+	}
+	else if (reqMethods == "POST")
+	{
+		return allowedMethods & POST;
+	}
+	else if (reqMethods == "DELETE")
+	{
+		return allowedMethods & DELETE;
+	}
+	return false;
 }
 
 void Router::routeRequest(const Request &request, Response &response)
 {
-    (void)response;
-    const std::string path = request.getUri();
-    const std::string trim = path.substr(0, path.find('/', 1));
+	(void)response;
+	const std::string path = request.getUri();
+	const std::string trim = path.substr(0, path.find('/', 1));
 
-    response.addHeader("Connection", "closed");
+	response.addHeader("Connection", "closed");
 
-    const std::map<std::string, RouteDetails *>::iterator routeIt = this->_routeTable.find(trim);
-    if (routeIt != this->_routeTable.end())
-    {
-        if (checkAllowedMethods(request.getMethod(), routeIt->second->allowedMethods) == false)
-        {
-            response.errorResponse(405, "Method not allowed");
-            return;
-        }
-        std::string fullPath = this->_projectDir + routeIt->second->root + request.getResource();
+	const std::map<std::string, RouteDetails *>::iterator routeIt = this->_routeTable.find(trim);
+	if (routeIt != this->_routeTable.end())
+	{
+		if (checkAllowedMethods(request.getMethod(), routeIt->second->allowedMethods) == false)
+		{
+			response.errorResponse(405, "Method not allowed");
+			return;
+		}
+		std::string fullPath = this->_projectDir + routeIt->second->root + request.getResource();
 
-        for (requestHandlerVecIt it = this->_requestHandlers->begin(); it != this->_requestHandlers->end(); it++)
-        {
-            if ((*it)->handleRequest(request, response, *routeIt->second, fullPath))
-                return;
-        }
-        if (request.getMethod() == "GET")
-        {
-            _logger.log("sending default plain success");
-            response.setStatusCode(200);
-            response.addHeader("Content-Type", "text/html");
-            response.addHeader("Connection", "closed");
-            response.setBody("<html>Welcome to the webserv, example.re homepage!</html>");
-            return;
-        }
-        // reach here if no handler found in the loop above
-        std::cout << "no handler found" << std::endl;
-        response.errorResponse(404, "no handler found");
-    }
-    else
-        response.errorResponse(404, "route not found in route table");
+		for (requestHandlerVecIt it = this->_requestHandlers->begin(); it != this->_requestHandlers->end(); it++)
+		{
+			if ((*it)->handleRequest(request, response, *routeIt->second, fullPath))
+				return;
+		}
+		if (request.getMethod() == "GET")
+		{
+			_logger.log("sending default plain success");
+			response.setStatusCode(200);
+			response.addHeader("Content-Type", "text/html");
+			response.addHeader("Connection", "closed");
+			response.setBody("<html>Welcome to the webserv, example.re homepage!</html>");
+			return;
+		}
+		// reach here if no handler found in the loop above
+		std::cout << "no handler found" << std::endl;
+		response.errorResponse(404, "no handler found");
+	}
+	else
+		response.errorResponse(404, "route not found in route table");
 }
 
 void Router::display(void) const
 {
-    std::cout << "Router Table:" << std::endl;
-    for (std::map<std::string, RouteDetails *>::const_iterator it = this->_routeTable.begin(); it != this->_routeTable.end(); it++)
-    {
-        std::cout << std::left << std::setw(20) << it->first << std::endl;
-    }
+	std::cout << "Router Table:" << std::endl;
+	for (std::map<std::string, RouteDetails *>::const_iterator it = this->_routeTable.begin(); it != this->_routeTable.end(); it++)
+	{
+		std::cout << std::left << std::setw(20) << it->first << std::endl;
+	}
 }
 
 //  /jaclyn/bombom.html
